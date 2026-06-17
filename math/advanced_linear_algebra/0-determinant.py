4e9bfc09f887a2e2115605f7d17bed5e7e3dda74
Rainer Staringer <rainer.staringer@ai.dlh.lu>
2026-06-17 12:16:36 +0200

    pycodestyle...

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -9,7 +9,7 @@ def determinant(matrix):
         return 1
     if (matrix == []
         or not (isinstance(matrix, list)
-            and all(isinstance(row, list) for row in matrix))):
+                and all(isinstance(row, list) for row in matrix))):
         raise TypeError("matrix must be a list of lists")
     n = len(matrix)
     if any(len(row) != n
