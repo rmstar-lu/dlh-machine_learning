8f6ce7107b15c71feefd7dc47ec52ee2116ee55b
Rainer Staringer <rstaring@student.42luxembourg.lu>
2026-06-18 10:28:44 +0200

    1-minor

---
+++ math/advanced_linear_algebra/1-minor.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+""" 1-minor module: Calculate the minor of a matrix. """
+
+
+def minor(matrix):
+    """ Calculate the minor of a matrix. """
+
+    def _det(matrix):
+        """ Calculate the determinant of a matrix. """
+
+        n = len(matrix)
+        if n == 0:
+            return 1
+        if n == 1:
+            return matrix[0][0]
+        i = 0
+        return sum(((-1) ** (i + j)) * matrix[i][j] * _det(
+                    [[matrix[mi][mj] for mj in range(n) if mj != j]
+                        for mi in range(n) if mi != i]
+               ) for j in range(n))
+
+    if (matrix == []
+        or not (isinstance(matrix, list)
+                and all(isinstance(row, list) for row in matrix))):
+        raise TypeError("matrix must be a list of lists")
+    n = len(matrix)
+    if (any(len(row) != n
+            or any(type(x) not in {int, float} for x in row)
+            for row in matrix)):
+        raise ValueError("matrix must be a non-empty square matrix")
+    return [[_det([[matrix[mi][mj] for mj in range(n) if mj != j]
+                   for mi in range(n) if mi != i])
+            for j in range(n)] for i in range(n)]
--- math/advanced_linear_algebra/README.md
+++ math/advanced_linear_algebra/README.md
@@ -3,3 +3,7 @@
 ## `0-determinant.py`
 
 Calculate the determinant of a matrix.
+
+## `1-minor.py`
+
+Calculate the minor matrix of a matrix.
