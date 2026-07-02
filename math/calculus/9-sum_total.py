7a02da2a3d22bda38cb3d6ea9c1f36f380f0a5e7
Rainer Staringer <rainer.staringer@ai.dlh.lu>
2026-07-02 08:57:39 +0200

    allowed floats but added check for < 1

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -5,6 +5,7 @@
 def summation_i_squared(n):
     """ Calculate the sum of i^2 for i from 1 to n """
 
-    if type(n) is not int:
+    if type(n) not in {int, float} or n < 1 or int(n) != n:
         return None
+    n = int(n)
     return int(n * (n + 1) * (2 * n + 1) / 6)
