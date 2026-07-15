efbb94fc745fb79650191826ab2607b8409820bc
Rainer Staringer <rainer.staringer@ai.dlh.lu>
2026-07-15 10:23:39 +0200

    added pmf

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -7,7 +7,11 @@ Module poisson, defines a class representing a Poisson distribution.
 class Poisson:
     """ Class representing a Poisson distribution. """
 
+    _E = 2.7182818285
+
     def __init__(self, data=None, lambtha=1.):
+        """ constructor """
+
         if data is None:
             if lambtha <= 0:
                 raise ValueError('lambtha must be a positive value')
@@ -18,3 +22,17 @@ class Poisson:
         else:
             raise TypeError('data must be a list')
         self.lambtha = float(lambtha)
+
+    def pmf(self, k):
+        """
+        Probability for x=k.
+        This will overflow for large k, but I am not allowed to use ln().
+        """
+
+        if k < 0:
+            return 0
+        pmf = (self.lambtha ** k) * (self._E ** -self.lambtha)
+        while k > 1:
+            pmf /= k
+            k -= 1
+        return pmf
