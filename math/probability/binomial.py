aecd8264165daeb95809644a46f29d06e5cc0949
Rainer Staringer <rainer.staringer@ai.dlh.lu>
2026-07-17 10:44:24 +0200

    added cdf

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -36,5 +36,15 @@ class Binomial:
             return 0
         n_choose_k = 1
         for i in range(k + 1, self.n + 1):
-            n_choose_k *= i / (i - k)
+            n_choose_k *= i
+        for i in range(2, self.n - k + 1):
+            n_choose_k //= i
         return n_choose_k * self.p ** k * (1 - self.p) ** (self.n - k)
+
+    def cdf(self, k):
+        """ Probability for x <= k """
+
+        k = int(k)
+        if k < 0:
+            return 0
+        return sum(self.pmf(j) for j in range(k + 1))
