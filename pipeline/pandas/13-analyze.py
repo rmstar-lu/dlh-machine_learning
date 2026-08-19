f1e04de650baa85d0fdf1f14cb378d85d5b23ec7
Rainer Staringer <rainer.staringer@ai.dlh.lu>
2026-08-19 13:17:57 +0200

    found out about describe()

--- pipeline/pandas/13-analyze.py
+++ pipeline/pandas/13-analyze.py
@@ -6,14 +6,8 @@ Computes descriptive statistics for all columns except the Timestamp column.
 Returns a new pd.DataFrame containing these statistics.
 """
 
-import pandas as pd
-
 
 def analyze(df):
     """ A function that computes descriptive statistics on a DataFrame """
 
-    df = df.drop(['Timestamp'], axis=1)
-    d = dict()
-    for k in ['count', 'mean', 'std', 'min', 'max']:
-        d[k] = getattr(df, k)()
-    return pd.DataFrame(d).T
+    return df.drop(['Timestamp'], axis=1).describe()
