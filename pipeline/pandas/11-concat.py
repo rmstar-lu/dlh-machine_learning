8f1e703bbb680011b08b4749e3dfe3872e64abc5
Rainer Staringer <rainer.staringer@ai.dlh.lu>
2026-08-19 12:15:36 +0200

    removed __import__ from comment as it confuses the checker

--- pipeline/pandas/11-concat.py
+++ pipeline/pandas/11-concat.py
@@ -8,7 +8,6 @@ timestamp 1417411920.
 Concatenates the selected rows from df2 to the top of df1 (coinbase).
 Adds keys to the concatenated data, labeling the rows from df2
 as bitstamp and the rows from df1 as coinbase.
-You should use index = __import__('10-index').index
 Returns the concatenated pd.DataFrame.
 """
 
